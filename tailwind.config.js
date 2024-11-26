b08a09dd76621491ec14e59185fd8666687a1385
Salahdine <salahdaha7@gmail.com>
2024-11-26 10:15:52 +0100

    FCWA-17 <Travaille sur L'interface de projet>

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'staduim-bg': "url('../assets/slide4.jpg')",
+      },
+
+
+    },
+  },
+  plugins: [],
+}
+
